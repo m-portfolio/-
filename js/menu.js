fc9be22b322aea2989c399039ad3c7910c19017b
m <62825155+m-portfolio@users.noreply.github.com>
2020-10-26 23:50:29 +0900

    Create menu.js

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
+
+
