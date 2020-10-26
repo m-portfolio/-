92acb58fd501a7204445dc10b04b32dfebcef9ee
m <62825155+m-portfolio@users.noreply.github.com>
2020-10-26 23:48:38 +0900

    Create dialog.js

---
+++ js/dialog.js
@@ -0,0 +1,260 @@
+// ------------------ illustrator ------------------
+// 1
+(function() {
+  const open = document.getElementById('open1');
+  const close = document.getElementById('close1');
+  const dialog = document.getElementById('dialog1');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+// 2
+(function() {
+  const open = document.getElementById('open2');
+  const close = document.getElementById('close2');
+  const dialog = document.getElementById('dialog2');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+// 3
+(function() {
+  const open = document.getElementById('open3');
+  const close = document.getElementById('close3');
+  const dialog = document.getElementById('dialog3');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+// 4
+(function() {
+  const open = document.getElementById('open4');
+  const close = document.getElementById('close4');
+  const dialog = document.getElementById('dialog4');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+
+
+
+// ------------------ photoshopr -------------------
+// 5
+(function() {
+  const open = document.getElementById('open5');
+  const close = document.getElementById('close5');
+  const dialog = document.getElementById('dialog5');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+
+// 6
+(function() {
+  const open = document.getElementById('open6');
+  const close = document.getElementById('close6');
+  const dialog = document.getElementById('dialog6');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+
+// 7
+(function() {
+  const open = document.getElementById('open7');
+  const close = document.getElementById('close7');
+  const dialog = document.getElementById('dialog7');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+
+// 8
+(function() {
+  const open = document.getElementById('open8');
+  const close = document.getElementById('close8');
+  const dialog = document.getElementById('dialog8');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+
+// ------------------- trace -------------------
+// 9
+(function() {
+  const open = document.getElementById('open9');
+  const close = document.getElementById('close9');
+  const dialog = document.getElementById('dialog9');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+// 10
+(function() {
+  const open = document.getElementById('open10');
+  const close = document.getElementById('close10');
+  const dialog = document.getElementById('dialog10');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
+// 11
+(function() {
+  const open = document.getElementById('open11');
+  const close = document.getElementById('close11');
+  const dialog = document.getElementById('dialog11');
+  
+  open.addEventListener('click', function() {
+    dialog.showModal();
+  });
+
+  close.addEventListener('click', function() {
+    dialog.close();
+  });
+  
+  dialog.addEventListener('click', function(event) {
+    if (event.target === dialog) {
+      dialog.close('cancelled');
+    }
+  });
+}());
+
+
+
